cb24ce733999a81a470b082c826e51fc3410e0f2
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2021-11-03 00:24:07 -0400

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,8 +7,6 @@ export default () => (
         <div><div>© MultiSend</div></div>
         <div><Link to="/donate">Donate</Link></div>
         <ul>
-            <li><a target="_blank" rel="noopener noreferrer" href="https://t.me/eltneg">Telegram</a></li>
-            <li><a target="_blank" rel="noopener noreferrer" href="https://etherscan.io/address/0x941f40c2955ee09ba638409f67ef27c531fc055c">Etherscan</a></li>
             <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/Multisend-ETH/client-v1">Github</a></li>
         </ul>
     </footer>
