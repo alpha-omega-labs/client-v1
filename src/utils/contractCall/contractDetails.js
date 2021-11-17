de1984fbce284b58004f02fdeb7f1c9315e325f3
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2021-11-17 01:15:27 -0500

    Update contractDetails.js

--- src/utils/contractCall/contractDetails.js
+++ src/utils/contractCall/contractDetails.js
@@ -1,4 +1,4 @@
-const contractAddress = "	0x8111805dd3918bb4e629a13602b292546dd3d0fa"
+const contractAddress = "0x8111805dd3918bb4e629a13602b292546dd3d0fa"
 const ABI = [
   {
     "name": "__init__",
